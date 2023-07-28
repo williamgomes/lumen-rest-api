da6477a0f693ed480a48efdf443a86734ec060a9
William Gomes <mijo.jfwg@gmail.com>
2023-07-28 17:01:08 +0200

    added database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Badge;
+use App\Models\Hotelier;
+use App\Models\Item;
+use App\Models\Location;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +18,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        if (count(Badge::all()->toArray()) === 0) {
+            $this->call(BadgesSeeder::class);
+            $this->command->info('Badges table seeded successfully!');
+        }
+
+        $countriesPath = database_path('migrations/sql/countries.sql');
+        DB::unprepared(file_get_contents($countriesPath));
+        $this->command->info('Countries table seeded successfully!');
+
+        $statesPath = database_path('migrations/sql/states.sql');
+        DB::unprepared(file_get_contents($statesPath));
+        $this->command->info('States table seeded successfully!');
+
+        $citiesPath = database_path('migrations/sql/city_part01.sql');
+        DB::unprepared(file_get_contents($citiesPath));
+        $citiesPath = database_path('migrations/sql/city_part02.sql');
+        DB::unprepared(file_get_contents($citiesPath));
+        $citiesPath = database_path('migrations/sql/city_part03.sql');
+        DB::unprepared(file_get_contents($citiesPath));
+        $citiesPath = database_path('migrations/sql/city_part04.sql');
+        DB::unprepared(file_get_contents($citiesPath));
+        $this->command->info('Cities table seeded successfully!');
+
+        Hotelier::factory(2)
+            ->create();
+        $this->command->info('Hotelier table seeded successfully!');
+
+        Item::factory(20)
+            ->has(Location::factory())
+            ->count(20)
+            ->create();
+        $this->command->info('Items & Locations table seeded successfully!');
     }
 }
