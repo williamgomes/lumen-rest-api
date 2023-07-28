1489b73ae5d2af77d50b74669ec2cfe3a48b68a5
William Gomes <mijo.jfwg@gmail.com>
2023-07-28 16:47:17 +0200

    added all necessary routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,39 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return "<h2>Welcome to Trivago Case Study</h2>";
+});
+
+$router->group(['prefix' => 'api/'], function ($router) {
+    $router->get('cities[/{perPage}]', 'CityController@index');
+    $router->get('states[/{perPage}]', 'StateController@index');
+    $router->get('countries[/{perPage}]', 'CountryController@index');
+
+    $router->group(['prefix' => 'hotelier/{hotelierId}', 'middleware' => 'fakeAuth'], function ($router) {
+        $router->get('items[/perPage/{perPage}]', 'ItemsController@index');
+        $router->get('items/ratings/{rating}[/perPage/{perPage}]', 'ItemsController@getByRating');
+        $router->get('items/cities/{cityId}[/perPage/{perPage}]', 'ItemsController@getByCity');
+        $router->get('items/badges/{badgeName}[/perPage/{perPage}]', 'ItemsController@getByBadge');
+        $router->get('items/{itemId}', [
+            'as' => 'items.show.one',
+            'uses' => 'ItemsController@show',
+        ]);
+        $router->post('items/', 'ItemsController@store');
+        $router->put('items/{itemId}', 'ItemsController@update');
+        $router->delete('items/{itemId}', 'ItemsController@delete');
+    });
+
+    //reservation group
+    $router->group(['prefix' => 'reservations'], function ($router) {
+        $router->post('/',                 [
+            'as' => 'items.create.reservation',
+            'uses' => 'ReservationController@store',
+        ]);
+        $router->get('{reservationId}',
+            [
+                'as' => 'reservation.show.one',
+                'uses' => 'ReservationController@show',
+            ]
+        );
+    });
 });
