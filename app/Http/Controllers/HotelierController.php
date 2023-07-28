d6fcc6f86df25526548e15fa1b064685f211b9be
William Gomes <mijo.jfwg@gmail.com>
2023-07-28 16:43:03 +0200

    added factory

--- app/Http/Controllers/HotelierController.php
+++ app/Http/Controllers/HotelierController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Http\Request;
 
 class HotelierController extends TrivagoController
 {
-
+    use HasFactory;
 }
